2fcff9b6915db5910ec89ac9106ce6eb3b30cc09
p-bucky <prashanttechjha@gmail.com>
2025-12-26 13:00:53 +0530

    [feat] completed and cancelled orders

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,3 +1,8 @@
 export const STATUS_PENDING: i32 = 0;
 export const STATUS_COMPLETED: i32 = 1;
 export const STATUS_FAILED: i32 = 2;
+
+export const DISPUTE_STATUS_RAISED: i32 = 1;
+export const DISPUTE_STATUS_SETTLED: i32 = 2;
+
+export const ORDER_STATUS_COMPLETED: i32 = 3;
---
+++ src/order.ts
@@ -0,0 +1,102 @@
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { OrderCompleted as OrderCompletedEvent} from "../generated/OrderFlowHelper/OrderFlowHelper";
+import { OrderDispute as OrderDisputeEvent } from "../generated/OrderProcessorFacet/OrderProcessorFacet";
+import { loadCircleMerchant, loadCircleMetrics } from "./utils";
+import { CircleMetrics } from "../generated/schema";
+import { DISPUTE_STATUS_RAISED, DISPUTE_STATUS_SETTLED } from "./constants/status";
+import { CancelledOrders as CancelledOrdersEvent } from "../generated/OrderFlowFacet/OrderFlowFacet";
+
+export function handleOrderCompleted(event: OrderCompletedEvent): void {
+  const merchant = loadCircleMerchant(
+    Bytes.fromHexString(event.params._order.acceptedMerchant.toString()),
+    event
+  );
+
+  const circle =
+    merchant && merchant.circle
+      ? Bytes.fromHexString(merchant.circle.toString())
+      : null;
+
+  if (!circle) return;
+
+  let circleMetrics = loadCircleMetrics(circle, event);
+
+  if (!circleMetrics) return;
+
+  circleMetrics.totalVolume = circleMetrics.totalVolume.plus(
+    event.params._order.amount
+  );
+  circleMetrics.completedOrdersCount = circleMetrics.completedOrdersCount.plus(
+    BigInt.fromI32(1)
+  );
+
+  circleMetrics.save();
+}
+
+/**
+ * Updates the dispute metrics for a given circle metrics and event
+ * @param circleMetrics - The circle metrics to update
+ * @param event - The event to update the dispute metrics for
+ */
+const updateDisputeMetrics = (
+  circleMetrics: CircleMetrics,
+  event: OrderDisputeEvent
+): void => {
+  if (event.params._order.disputeInfo.status === DISPUTE_STATUS_RAISED) {
+    circleMetrics.raisedDisputesCount = circleMetrics.raisedDisputesCount.plus(
+      BigInt.fromI32(1)
+    );
+  } else if (event.params._order.disputeInfo.status === DISPUTE_STATUS_SETTLED) {
+    circleMetrics.raisedDisputesCount = circleMetrics.raisedDisputesCount.minus(
+      BigInt.fromI32(1)
+    );
+    circleMetrics.resolvedDisputesCount =
+      circleMetrics.resolvedDisputesCount.plus(BigInt.fromI32(1));
+  }
+};
+
+export function handleOrderDispute(event: OrderDisputeEvent): void {
+  const merchant = loadCircleMerchant(
+    Bytes.fromHexString(event.params._order.acceptedMerchant.toString()),
+    event
+  );
+
+  const circle =
+    merchant && merchant.circle
+      ? Bytes.fromHexString(merchant.circle.toString())
+      : null;
+
+  if (!circle) return;
+
+  let circleMetrics = loadCircleMetrics(circle, event);
+
+  if (!circleMetrics) return;
+
+  updateDisputeMetrics(circleMetrics, event);
+
+  circleMetrics.save();
+}
+
+export function handleCancelledOrders(event: CancelledOrdersEvent): void {
+  const merchant = loadCircleMerchant(
+    Bytes.fromHexString(event.params._order.acceptedMerchant.toString()),
+    event
+  );
+
+  const circle =
+    merchant && merchant.circle
+      ? Bytes.fromHexString(merchant.circle.toString())
+      : null;
+
+  if (!circle) return;
+
+  let circleMetrics = loadCircleMetrics(circle, event);
+
+  if (!circleMetrics) return;
+
+  circleMetrics.cancelledOrdersCount = circleMetrics.cancelledOrdersCount.plus(
+    BigInt.fromI32(1)
+  );
+  
+  circleMetrics.save();
+}
