77fce25909d9989dff012b34b446d007367ec0e2
ekdromoi <warbornleonidas@gmail.com>
2026-04-14 23:25:08 +0530

    [feat] Insurance Pool

---
+++ src/insurance-claim.ts
@@ -0,0 +1,71 @@
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import {
+  ClaimSubmitted as ClaimSubmittedEvent,
+  ClaimApproved as ClaimApprovedEvent,
+  ClaimRejected as ClaimRejectedEvent,
+} from "../generated/InsuranceClaimFacet/InsuranceClaimFacet";
+import { loadInsuranceClaim } from "./lib";
+
+export function handleClaimSubmitted(event: ClaimSubmittedEvent): void {
+  const claim = event.params.claim;
+
+  const entity = loadInsuranceClaim(
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.claimId)),
+    event,
+  );
+
+  entity.claimId = event.params.claimId;
+  entity.circleId = event.params.circleId;
+  entity.circle = Bytes.fromByteArray(Bytes.fromBigInt(event.params.circleId));
+  entity.claimant = event.params.claimant;
+
+  entity.claimType = claim.claimType;
+  entity.status = claim.status;
+
+  entity.fiatAmount = claim.fiatAmount;
+  entity.usdcAmount = claim.usdcAmount;
+  entity.accountNo = claim.accountNo;
+
+  entity.orderId = claim.orderId;
+  entity.order = Bytes.fromByteArray(Bytes.fromBigInt(claim.orderId));
+
+  entity.merchant = event.params.claimant;
+
+  entity.currency = Bytes.fromByteArray(claim.currency);
+  entity.submittedAt = claim.submittedAt;
+  entity.reviewedAt = claim.reviewedAt;
+  entity.resolver = claim.resolver;
+
+  entity.requiresSuperAdminApproval = claim.requiresSuperAdminApproval;
+  entity.superAdminApproved = claim.superAdminApproved;
+
+  entity.save();
+}
+
+export function handleClaimApproved(event: ClaimApprovedEvent): void {
+  const entity = loadInsuranceClaim(
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.claimId)),
+    event,
+  );
+
+  // ClaimStatus.APPROVED = 2
+  entity.status = 2;
+  entity.resolver = event.params.resolver;
+  entity.reviewedAt = event.block.timestamp;
+
+  entity.save();
+}
+
+export function handleClaimRejected(event: ClaimRejectedEvent): void {
+  const entity = loadInsuranceClaim(
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.claimId)),
+    event,
+  );
+
+  // ClaimStatus.REJECTED = 3
+  entity.status = 3;
+  entity.resolver = event.params.resolver;
+  entity.reviewedAt = event.block.timestamp;
+
+  entity.save();
+}
